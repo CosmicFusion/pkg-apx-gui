c94564cd15d92181831f7d690a5c428b46e29606
Ward Nakchbandi (Cosmic Fusion) <83735213+CosmicFusion@users.noreply.github.com>
2023-08-14 18:33:29 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,6 +1,10 @@
 # Clone Upstream
 git clone https://github.com/Vanilla-OS/apx-gui ./vanilla-apx-gui
 cd ./vanilla-apx-gui
+rm -rfv debian/changelog
+touch debian/changelog
+echo -e "vanilla-apx-gui (1.0.2-99pika2.lunar) lunar; urgency=medium\n\n  * New Release\n\n -- Ward Nakchbandi <hotrod.master@hotmail.com> Sat, 01 Oct 2022 14:50:00 +0200" > debian/changelog
+
 
 # Get build deps
 apt-get build-dep ./ -y
